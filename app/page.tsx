5f790dea25b90395e1909a31a86b83d2410f60f3
sobotkakrystof5-png <sobotkakrystof5@gmail.com>
2026-08-31 18:15:27 +0200

    Revert "feat(reference): sjednotit sekci referencí do jednoho panelu místo karuselu"
    This reverts commit d01daaaa601c544958322aca2bb9f45d31d2ae54.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import IntroAnimation from "@/components/IntroAnimation";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import SocialProof from "@/components/SocialProof";
-import TestimonialWall from "@/components/TestimonialWall";
+import TestimonialCarousel from "@/components/TestimonialCarousel";
 import StatementBlock from "@/components/StatementBlock";
 import HomeExplore from "@/components/HomeExplore";
 import Contact from "@/components/Contact";
@@ -33,7 +33,7 @@ export default function Home() {
       <main id="main-content">
         <Hero />
         <SocialProof />
-        <TestimonialWall />
+        <TestimonialCarousel />
         <StatementBlock />
         <HomeExplore />
         <Contact headingLevel="h2" />
